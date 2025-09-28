a1924ebfe4cd8472727cbf672aefc67dab6c0d43
ahmdmoadi <ahmdmoadi@gmail.com>
2025-09-28 19:50:18 +0800

    500 errs go away

--- envr.js
+++ envr.js
@@ -1,7 +1,9 @@
 const {readFileSync} = require("fs");
 
-const envfile = readFileSync(".env").toString();
-
+try {const envfile = readFileSync(".env").toString();
+} catch(e) {
+    console.warn(".env not found! better be in vercel!")
+}
 const lines = envfile.replace(/\r/g,"").split(/\n/i);
 
 lines.forEach(line => {
