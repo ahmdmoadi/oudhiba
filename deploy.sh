b3da6639208f573eafabf855254c04b5c54022df
ahmdmoadi <ahmdmoadi@gmail.com>
2025-09-28 20:08:22 +0800

    BASHDEPLOY

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+git add .
+git commit -m "BASHDEPLOY"
+git push
+npx vercel deploy --prod
--- envr.js
+++ envr.js
@@ -1,12 +1,16 @@
 const {readFileSync} = require("fs");
 
-try {const envfile = readFileSync(".env").toString();
+let envfile = "";
+
+try {envfile = readFileSync(".env").toString();
 } catch(e) {
     // console.warn(".env not found! better be in vercel!")
 }
 const lines = envfile.replace(/\r/g,"").split(/\n/i);
 
-lines.forEach(line => {
-    const [k, v] = line.split("=");
-    process.env[k] = v;
-});
+if(!!lines) {
+    lines.forEach(line => {
+        const [k, v] = line.split("=");
+        process.env[k] = v;
+    });
+}
